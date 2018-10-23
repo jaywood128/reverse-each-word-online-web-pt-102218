9738521bb5dead2acc83c78c8aca508b86e40f32
Johnathon Wood <jaywood128@gmail.com>
2018-10-23 16:45:14 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
-  string = "Hello, there and how are you?"
   array = string.split
-  array.each {|word| word.reverse}
+  array.map {|word| word.reverse}
   reverse = array.reverse
   reverse.join
 end
