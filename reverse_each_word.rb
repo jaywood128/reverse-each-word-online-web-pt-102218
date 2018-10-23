4daafe5c8c93182ec6c291de1c1fd7a0cdfd351d
Johnathon Wood <jaywood128@gmail.com>
2018-10-23 16:34:53 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   string = "Hello, there and how are you?"
   array = string.split
-  array.each {|word| reverse.word}
+  array.each {|word| word.reverse}
   reverse = array.reverse
   reverse.join
 end
